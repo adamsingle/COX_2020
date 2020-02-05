e3bf389b1bd87bb9aaa8c64a9d8f1806c8117c71
Bugenoid <bugenoid@gmail.com>
2020-02-05 15:20:11 +1100

    Update Image_Track.cs

--- Assets/Team2_Trackers/Image_Track.cs
+++ Assets/Team2_Trackers/Image_Track.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
 
 public class Image_Track : MonoBehaviour
 {
+
+    private ARTrackedImageManager aRTrackedImageManager;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
